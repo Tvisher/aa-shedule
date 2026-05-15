7cd6d51f2fdbd867b6314119c0cc8fd6144b0fb5
Tvisher <tvisherfine@gmail.com>
2026-05-15 15:57:04 +0500

    !

--- src/main.js
+++ src/main.js
@@ -2,15 +2,15 @@ import { createApp } from 'vue';
 import { createPinia } from 'pinia';
 import '@vuepic/vue-datepicker/dist/main.css';
 import 'vue-skeletor/dist/vue-skeletor.css';
-if (import.meta.env.DEV) {
-    import('./assets/css/aos.css');
-    import('./assets/css/fancybox.css');
-    import('./assets/css/main.min.css');
-    import('./assets/css/normalize.min.css');
-    import('./assets/css/overlayscrollbars.min.css');
-    import('./assets/css/plyr.css');
-    import('./assets/css/swiper-bundle.css');
-}
+// if (import.meta.env.DEV) {
+import('./assets/css/aos.css');
+import('./assets/css/fancybox.css');
+import('./assets/css/main.min.css');
+import('./assets/css/normalize.min.css');
+import('./assets/css/overlayscrollbars.min.css');
+import('./assets/css/plyr.css');
+import('./assets/css/swiper-bundle.css');
+// }
 import './style.scss'
 import App from './App.vue'
 
